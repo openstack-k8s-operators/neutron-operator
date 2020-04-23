f9dc66dd7f0fb4c9a1ea25e8f537e15d9dfd4956
Kamil Sambor <sambork@localhost.localdomain>
2020-04-23 16:01:06 +0200

    Format code with goimports
    - reformat code with goimports
    - add info about goimports into README

--- pkg/apis/neutron/v1/neutronsriovagent_types.go
+++ pkg/apis/neutron/v1/neutronsriovagent_types.go
@@ -7,23 +7,23 @@ import (
 // NeutronSriovAgentSpec defines the desired state of NeutronSriovAgent
 // +k8s:openapi-gen=true
 type NeutronSriovAgentSpec struct {
-        // Label is the value of the 'daemon=' label to set on a node that should run the daemon
-        Label string `json:"label"`
-        // Image is the Docker image to run for the daemon
-        NeutronSriovImage string `json:"neutronSriovImage"`
-        // RabbitMQ transport URL String
-        RabbitTransportUrl string `json:"rabbitTransportUrl"`
-        // Debug
-        Debug string `json:"debug,omitempty"`
+	// Label is the value of the 'daemon=' label to set on a node that should run the daemon
+	Label string `json:"label"`
+	// Image is the Docker image to run for the daemon
+	NeutronSriovImage string `json:"neutronSriovImage"`
+	// RabbitMQ transport URL String
+	RabbitTransportUrl string `json:"rabbitTransportUrl"`
+	// Debug
+	Debug string `json:"debug,omitempty"`
 }
 
 // NeutronSriovAgentStatus defines the observed state of NeutronSriovAgent
 // +k8s:openapi-gen=true
 type NeutronSriovAgentStatus struct {
-        // Count is the number of nodes the daemon is deployed to
-        Count int32 `json:"count"`
-        // Daemonset hash used to detect changes
-        DaemonsetHash string `json:"daemonsetHash"`
+	// Count is the number of nodes the daemon is deployed to
+	Count int32 `json:"count"`
+	// Daemonset hash used to detect changes
+	DaemonsetHash string `json:"daemonsetHash"`
 }
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
