8d2edfaed86e9550b65f0f42c39e4505cb568c33
Kamil Sambor <ksambor@redhat.com>
2020-07-15 10:55:49 +0200

    Add scipts and improve Makefile to improve installation

---
+++ build_image.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+oc create -f deploy/crds/neutron_v1_neutronovsagent_crd.yaml
+oc create -f deploy/crds/neutron_v1_neutronsriovagent_crd.yaml
+oc create -f deploy/crds/neutron.openstack.org_ovsnodeosps_crd.yaml
+oc create -f deploy/crds/neutron.openstack.org_ovncontrollers_crd.yaml
+make
+operator-sdk build --image-builder buildah "${REGISTRY:-quay.io/openstack-k8s-operators/neutron-operator:v0.0.3}"
+sed -i 's|REPLACE_IMAGE|"${REGISTRY:-quay.io/openstack-k8s-operators/neutron-operator:v0.0.3}"g' deploy/operator.yaml
+podman push --authfile "${REGISTRY_AUTH}" "${REGISTRY:-quay.io/openstack-k8s-operators/neutron-operator:v0.0.3}"
---
+++ cleanup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+oc delete -f deploy/crds/neutron_v1_neutronovsagent_cr.yaml
+oc delete -f deploy/crds/neutron_v1_neutronsriovagent_cr.yaml
+oc delete -f deploy/crds/neutron.openstack.org_v1_ovsnodeosp_cr.yaml
+oc delete -f deploy/crds/neutron.openstack.org_v1_ovncontroller_cr.yaml
+oc delete -f deploy/operator.yaml
+oc delete -f deploy/role.yaml
+oc delete -f deploy/cluster_role.yaml
+oc delete -f deploy/role_binding.yaml
+oc delete -f deploy/cluster_role_binding.yaml
+oc delete -f deploy/scc.yaml
+oc delete -f deploy/service_account.yaml
+oc delete -f deploy/crds/neutron_v1_neutronovsagent_crd.yaml
+oc delete -f deploy/crds/neutron_v1_neutronsriovagent_crd.yaml
+oc delete -f deploy/crds/neutron.openstack.org_ovsnodeosps_crd.yaml
+oc delete -f deploy/crds/neutron.openstack.org_ovncontrollers_crd.yaml
