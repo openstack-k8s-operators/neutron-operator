0e8ca1c25353e333dbd70b2ee557d49fbbfd14f8
Dan Prince <dprince@redhat.com>
2020-07-16 12:57:04 -0400

    Use object_util and template_util from lib-common

--- pkg/neutronsriovagent/configmap.go
+++ pkg/neutronsriovagent/configmap.go
@@ -1,8 +1,8 @@
 package neutronsriovagent
 
 import (
+	util "github.com/openstack-k8s-operators/lib-common/pkg/util"
 	neutronv1 "github.com/openstack-k8s-operators/neutron-operator/pkg/apis/neutron/v1"
-	util "github.com/openstack-k8s-operators/neutron-operator/pkg/util"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
