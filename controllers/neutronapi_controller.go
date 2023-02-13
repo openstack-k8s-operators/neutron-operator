7dee9405b745ae02bbafd2ed5ce324ef92a2b0f5
Lewis Denny <ldenny@redhat.com>
2023-02-13 21:11:13 +1000

    Replace deprecated functions
    db.WaitForDBCreated -> db.WaitForDBCreatedWithTimeout
    db.CreateOrPatchDB -> db.CreateOrPatchDBByName
    
    Functionality stays the same

--- controllers/neutronapi_controller.go
+++ controllers/neutronapi_controller.go
@@ -272,9 +272,10 @@ func (r *NeutronAPIReconciler) reconcileInit(
 		},
 	)
 	// create or patch the DB
-	ctrlResult, err := db.CreateOrPatchDB(
+	ctrlResult, err := db.CreateOrPatchDBByName(
 		ctx,
 		helper,
+		"openstack",
 	)
 
 	if err != nil {
@@ -295,7 +296,7 @@ func (r *NeutronAPIReconciler) reconcileInit(
 		return ctrlResult, nil
 	}
 	// wait for the DB to be setup
-	ctrlResult, err = db.WaitForDBCreated(ctx, helper)
+	ctrlResult, err = db.WaitForDBCreatedWithTimeout(ctx, helper, time.Second*5)
 	if err != nil {
 		instance.Status.Conditions.Set(condition.FalseCondition(
 			condition.DBReadyCondition,
