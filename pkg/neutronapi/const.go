379fbdc557f8ed7e0d6ef4e3b50124c75c06892e
Takashi Kajinami <tkajinam@redhat.com>
2023-04-26 15:10:35 +0900

    Remove unused constant for admin endpoint
    The constant has not been used since we stopped creating admin
    endpoint.

--- pkg/neutronapi/const.go
+++ pkg/neutronapi/const.go
@@ -18,8 +18,6 @@ const (
 	// Database -
 	Database = "neutron"
 
-	// NeutronAdminPort -
-	NeutronAdminPort int32 = 9696
 	// NeutronPublicPort -
 	NeutronPublicPort int32 = 9696
 	// NeutronInternalPort -
