b30132a0c95aaba77009e3b22327e71ad295b7f7
Ihar Hrachyshka <ihrachys@redhat.com>
2023-10-05 14:03:23 +0000

    Run neutronapi / db-sync as neutron:neutron
    Now that we removed kolla bits from the startup path, we can unprivilege the
    containers.
    
    Note: since we still may mount HostPaths, we have to stick to hostmount-anyuid
    for now. If this interface is revisited and e.g. switched to PVCs, then we
    should be able to tighten SCC a bit more.

---
+++ pkg/neutronapi/scc.go
@@ -0,0 +1,22 @@
+package neutronapi
+
+import corev1 "k8s.io/api/core/v1"
+
+func getNeutronSecurityContext() *corev1.SecurityContext {
+	falseVal := false
+	trueVal := true
+	runAsUser := int64(NeutronUid)
+	runAsGroup := int64(NeutronGid)
+
+	return &corev1.SecurityContext{
+		RunAsUser:                &runAsUser,
+		RunAsGroup:               &runAsGroup,
+		RunAsNonRoot:             &trueVal,
+		AllowPrivilegeEscalation: &falseVal,
+		Capabilities: &corev1.Capabilities{
+			Drop: []corev1.Capability{
+				"ALL",
+			},
+		},
+	}
+}
