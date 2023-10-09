openstack-k8s-operators/neutron-operator

commit 7a750f104a2a6d94ee7bfdeff1fb699e254c35e5

Remove kolla bits from service configuration path
Signed-off-by: Ihar Hrachyshka <ihrachys@redhat.com>

controllers/neutronapi_controller.go
@@ -34,7 +34,6 @@ import (
 	keystonev1 "github.com/openstack-k8s-operators/keystone-operator/api/v1beta1"
 	"github.com/openstack-k8s-operators/lib-common/modules/common"
 	"github.com/openstack-k8s-operators/lib-common/modules/common/condition"
-	"github.com/openstack-k8s-operators/lib-common/modules/common/configmap"
 	"github.com/openstack-k8s-operators/lib-common/modules/common/deployment"
 	"github.com/openstack-k8s-operators/lib-common/modules/common/endpoint"
 	"github.com/openstack-k8s-operators/lib-common/modules/common/env"
@@ -71,7 +70,6 @@ type NeutronAPIReconciler struct {
 // +kubebuilder:rbac:groups=neutron.openstack.org,resources=neutronapis/status,verbs=get;update;patch
 // +kubebuilder:rbac:groups=neutron.openstack.org,resources=neutronapis/finalizers,verbs=update
 // +kubebuilder:rbac:groups=core,resources=secrets,verbs=get;list;watch;create;update;patch;delete;
-// +kubebuilder:rbac:groups=core,resources=configmaps,verbs=get;list;watch;create;update;patch;delete;
 // +kubebuilder:rbac:groups=core,resources=services,verbs=get;list;watch;create;patch;update;delete;
 // +kubebuilder:rbac:groups=core,resources=pods,verbs=get;list;
 // +kubebuilder:rbac:groups=batch,resources=jobs,verbs=get;list;watch;create;patch;update;delete;
@@ -201,7 +199,6 @@ func (r *NeutronAPIReconciler) SetupWithManager(mgr ctrl.Manager) error {
 		Owns(&batchv1.Job{}).
 		Owns(&corev1.Service{}).
 		Owns(&corev1.Secret{}).
-		Owns(&corev1.ConfigMap{}).
 		Owns(&appsv1.Deployment{}).
 		Owns(&keystonev1.KeystoneService{}).
 		Owns(&keystonev1.KeystoneEndpoint{}).
@@ -274,6 +271,8 @@ func (r *NeutronAPIReconciler) reconcileInit(
 	helper *helper.Helper,
 	serviceLabels map[string]string,
 	serviceAnnotations map[string]string,
+	ospSecret *corev1.Secret,
+	secretVars map[string]env.Setter,
 ) (ctrl.Result, error) {
 	r.Log.Info("Reconciling Service init")
 
@@ -334,6 +333,45 @@ func (r *NeutronAPIReconciler) reconcileInit(
 	instance.Status.DatabaseHostname = db.GetDatabaseHostname()
 	instance.Status.Conditions.MarkTrue(condition.DBReadyCondition, condition.DBReadyMessage)
 	// create neutron DB - end
+
+	// Create Secrets required as input for the Service and calculate an overall hash of hashes
+	//
+
+	//
+	// create Secret required for neutronapi and dbsync input. It contains minimal neutron config required
+	// to get the service up, user can add additional files to be added to the service.
+	err = r.generateServiceSecrets(ctx, helper, instance, ospSecret, &secretVars)
+	if err != nil {
+		instance.Status.Conditions.Set(condition.FalseCondition(
+			condition.ServiceConfigReadyCondition,
+			condition.ErrorReason,
+			condition.SeverityWarning,
+			condition.ServiceConfigReadyErrorMessage,
+			err.Error()))
+		return ctrl.Result{}, err
+	}
+
+	//
+	// create hash over all the different input resources to identify if any those changed
+	// and a restart/recreate is required.
+	//
+	hashChanged, err := r.createHashOfInputHashes(ctx, instance, secretVars)
+	if err != nil {
+		instance.Status.Conditions.Set(condition.FalseCondition(
+			condition.ServiceConfigReadyCondition,
+			condition.ErrorReason,
+			condition.SeverityWarning,
+			condition.ServiceConfigReadyErrorMessage,
+			err.Error()))
+		return ctrl.Result{}, err
+	} else if hashChanged {
+		// Hash changed and instance status should be updated (which will be done by main defer func),
+		// so we need to return and reconcile again
+		return ctrl.Result{}, nil
+	}
+	// Create Secrets - end
+
+	instance.Status.Conditions.MarkTrue(condition.ServiceConfigReadyCondition, condition.ServiceConfigReadyMessage)
 
 	//
 	// run Neutron db sync
@@ -591,8 +629,7 @@ func (r *NeutronAPIReconciler) reconcileNormal(ctx context.Context, instance *ne
 		return rbacResult, nil
 	}
 
-	// ConfigMap
-	configMapVars := make(map[string]env.Setter)
+	secretVars := make(map[string]env.Setter)
 
 	//
 	// create RabbitMQ transportURL CR and get the actual URL from the associated secret that is created
@@ -650,7 +687,7 @@ func (r *NeutronAPIReconciler) reconcileNormal(ctx context.Context, instance *ne
 			err.Error()))
 		return ctrl.Result{}, err
 	}
-	configMapVars[transportURLSecret.Name] = env.SetValue(hash)
+	secretVars[transportURLSecret.Name] = env.SetValue(hash)
 
 	// run check TransportURL secret - end
 
@@ -679,59 +716,16 @@ func (r *NeutronAPIReconciler) reconcileNormal(ctx context.Context, instance *ne
 			err.Error()))
 		return ctrl.Result{}, err
 	}
-	configMapVars[ospSecret.Name] = env.SetValue(hash)
+	secretVars[ospSecret.Name] = env.SetValue(hash)
 
 	instance.Status.Conditions.MarkTrue(condition.InputReadyCondition, condition.InputReadyMessage)
 	// run check OpenStack secret - end
 
-	err = r.reconcileExternalSecrets(ctx, helper, instance, &configMapVars)
+	err = r.reconcileExternalSecrets(ctx, helper, instance, &secretVars)
 	if err != nil {
 		r.Log.Error(err, "Failed to reconcile external Secrets")
 		return ctrl.Result{}, err
 	}
-
-	//
-	// Create ConfigMaps and Secrets required as input for the Service and calculate an overall hash of hashes
-	//
-
-	//
-	// create Configmap required for neutron input
-	// - %-scripts configmap holding scripts to e.g. bootstrap the service
-	// - %-config configmap holding minimal neutron config required to get the service up, user can add additional files to be added to the service
-	// - parameters which has passwords gets added from the OpenStack secret via the init container
-	//
-	err = r.generateServiceConfigMaps(ctx, helper, instance, &configMapVars)
-	if err != nil {
-		instance.Status.Conditions.Set(condition.FalseCondition(
-			condition.ServiceConfigReadyCondition,
-			condition.ErrorReason,
-			condition.SeverityWarning,
-			condition.ServiceConfigReadyErrorMessage,
-			err.Error()))
-		return ctrl.Result{}, err
-	}
-
-	//
-	// create hash over all the different input resources to identify if any those changed
-	// and a restart/recreate is required.
-	//
-	inputHash, hashChanged, err := r.createHashOfInputHashes(ctx, instance, configMapVars)
-	if err != nil {
-		instance.Status.Conditions.Set(condition.FalseCondition(
-			condition.ServiceConfigReadyCondition,
-			condition.ErrorReason,
-			condition.SeverityWarning,
-			condition.ServiceConfigReadyErrorMessage,
-			err.Error()))
-		return ctrl.Result{}, err
-	} else if hashChanged {
-		// Hash changed and instance status should be updated (which will be done by main defer func),
-		// so we need to return and reconcile again
-		return ctrl.Result{}, nil
-	}
-	// Create ConfigMaps and Secrets - end
-
-	instance.Status.Conditions.MarkTrue(condition.ServiceConfigReadyCondition, condition.ServiceConfigReadyMessage)
 
 	//
 	// TODO check when/if Init, Update, or Upgrade should/could be skipped
@@ -771,7 +765,7 @@ func (r *NeutronAPIReconciler) reconcileNormal(ctx context.Context, instance *ne
 	}
 
 	// Handle service init
-	ctrlResult, err := r.reconcileInit(ctx, instance, helper, serviceLabels, serviceAnnotations)
+	ctrlResult, err := r.reconcileInit(ctx, instance, helper, serviceLabels, serviceAnnotations, ospSecret, secretVars)
 	if err != nil {
 		return ctrlResult, err
 	} else if (ctrlResult != ctrl.Result{}) {
@@ -795,6 +789,11 @@ func (r *NeutronAPIReconciler) reconcileNormal(ctx context.Context, instance *ne
 	}
 
 	// Define a new Deployment object
+	inputHash, ok := instance.Status.Hash[common.InputHashName]
+	if !ok {
+		return ctrlResult, fmt.Errorf("Failed to fetch input hash for Neutron deployment")
+	}
+
 	deplDef := neutronapi.Deployment(instance, inputHash, serviceLabels, serviceAnnotations)
 	depl := deployment.NewDeployment(
 		deplDef,
@@ -916,13 +915,29 @@ func (r *NeutronAPIReconciler) reconcileExternalMetadataAgentSecret(
 	return nil
 }
 
+func (r *NeutronAPIReconciler) getTransportURL(
+	ctx context.Context,
+	h *helper.Helper,
+	instance *neutronv1beta1.NeutronAPI,
+) (string, error) {
+	transportURLSecret, _, err := secret.GetSecret(ctx, h, instance.Status.TransportURLSecret, instance.Namespace)
+	if err != nil {
+		return "", err
+	}
+	transportURL, ok := transportURLSecret.Data["transport_url"]
+	if !ok {
+		return "", fmt.Errorf("No transport_url key found in Transport Secret")
+	}
+	return string(transportURL), nil
+}
+
 func (r *NeutronAPIReconciler) reconcileExternalSriovAgentSecret(
 	ctx context.Context,
 	h *helper.Helper,
 	instance *neutronv1beta1.NeutronAPI,
 	envVars *map[string]env.Setter,
 ) error {
-	transportURLSecret, _, err := secret.GetSecret(ctx, h, instance.Status.TransportURLSecret, instance.Namespace)
+	transportURL, err := r.getTransportURL(ctx, h, instance)
 	if err != nil {
 		err = r.deleteExternalSecret(ctx, h, instance, getSriovAgentSecretName(instance))
 		if err != nil {
@@ -930,15 +945,7 @@ func (r *NeutronAPIReconciler) reconcileExternalSriovAgentSecret(
 		}
 		return nil
 	}
-	transportURL, ok := transportURLSecret.Data["transport_url"]
-	if !ok {
-		err = r.deleteExternalSecret(ctx, h, instance, getSriovAgentSecretName(instance))
-		if err != nil {
-			return fmt.Errorf("Failed to delete Neutron SR-IOV Agent external Secret: %w", err)
-		}
-		return nil
-	}
-	err = r.ensureExternalSriovAgentSecret(ctx, h, instance, string(transportURL), envVars)
+	err = r.ensureExternalSriovAgentSecret(ctx, h, instance, transportURL, envVars)
 	if err != nil {
 		return fmt.Errorf("Failed to ensure Neutron SR-IOV Agent external Secret: %w", err)
 	}
@@ -1109,15 +1116,18 @@ func (r *NeutronAPIReconciler) ensureExternalDhcpAgentSecret(
 	return r.ensureExternalSecret(ctx, h, instance, secretName, templates, templateParameters, envVars)
 }
 
-// generateServiceConfigMaps - create create configmaps which hold scripts and service configuration
+// generateServiceSecrets - create secrets which service configuration
+// TODO(ihar) we may want to split generation of config for db-sync and for neutronapi main pod, which
+// would allow the operator to proceed with db-sync without waiting for other configuration values
 // TODO add DefaultConfigOverwrite
-func (r *NeutronAPIReconciler) generateServiceConfigMaps(
+func (r *NeutronAPIReconciler) generateServiceSecrets(
 	ctx context.Context,
 	h *helper.Helper,
 	instance *neutronv1beta1.NeutronAPI,
+	ospSecret *corev1.Secret,
 	envVars *map[string]env.Setter,
 ) error {
-	// Create/update configmaps from templates
+	// Create/update secrets from templates
 	cmLabels := labels.GetLabels(instance, labels.GetGroupLabel(neutronapi.ServiceName), map[string]string{})
 
 	nbCluster, err := ovnclient.GetDBClusterByType(ctx, h, instance.Namespace, map[string]string{}, ovnclient.NBDBType)
@@ -1159,26 +1169,37 @@ func (r *NeutronAPIReconciler) generateServiceConfigMaps(
 	if err != nil {
 		return err
 	}
+
+	transportURL, err := r.getTransportURL(ctx, h, instance)
+	if err != nil {
+		return err
+	}
+
 	templateParameters := make(map[string]interface{})
 	templateParameters["ServiceUser"] = instance.Spec.ServiceUser
 	templateParameters["KeystoneInternalURL"] = keystoneInternalURL
 	templateParameters["KeystonePublicURL"] = keystonePublicURL
+	templateParameters["TransportURL"] = transportURL
 
+	// Other OpenStack services
+	servicePassword := string(ospSecret.Data[instance.Spec.PasswordSelectors.Service])
+	databasePassword := string(ospSecret.Data[instance.Spec.PasswordSelectors.Database])
+	templateParameters["NovaPassword"] = servicePassword
+	templateParameters["ServicePassword"] = servicePassword
+
+	// Database
+	templateParameters["DbHost"] = instance.Status.DatabaseHostname
+	templateParameters["DbUser"] = instance.Spec.DatabaseUser
+	templateParameters["DbPassword"] = databasePassword
+	templateParameters["Db"] = neutronapi.Database
+
+	// OVN
 	templateParameters["NBConnection"] = nbEndpoint
 	templateParameters["SBConnection"] = sbEndpoint
 
-	cms := []util.Template{
-		// ScriptsConfigMap
+	secrets := []util.Template{
 		{
-			Name:         fmt.Sprintf("%s-scripts", instance.Name),
-			Namespace:    instance.Namespace,
-			Type:         util.TemplateTypeScripts,
-			InstanceType: instance.Kind,
-			Labels:       cmLabels,
-		},
-		// ConfigMap
-		{
-			Name:          fmt.Sprintf("%s-config-data", instance.Name),
+			Name:          fmt.Sprintf("%s-config", instance.Name),
 			Namespace:     instance.Namespace,
 			Type:          util.TemplateTypeConfig,
 			InstanceType:  instance.Kind,
@@ -1187,28 +1208,28 @@ func (r *NeutronAPIReconciler) generateServiceConfigMaps(
 			ConfigOptions: templateParameters,
 		},
 	}
-	return configmap.EnsureConfigMaps(ctx, h, instance, cms, envVars)
+	return secret.EnsureSecrets(ctx, h, instance, secrets, envVars)
 }
 
 // createHashOfInputHashes - creates a hash of hashes which gets added to the resources which requires a restart
 // if any of the input resources change, like configs, passwords, ...
 //
-// returns the hash, whether the hash changed (as a bool) and any error
+// returns whether the hash changed (as a bool) and any error
 func (r *NeutronAPIReconciler) createHashOfInputHashes(
 	ctx context.Context,
 	instance *neutronv1beta1.NeutronAPI,
 	envVars map[string]env.Setter,
-) (string, bool, error) {
+) (bool, error) {
 	var hashMap map[string]string
 	changed := false
 	mergedMapVars := env.MergeEnvs([]corev1.EnvVar{}, envVars)
 	hash, err := util.ObjectHash(mergedMapVars)
 	if err != nil {
-		return hash, changed, err
+		return changed, err
 	}
 	if hashMap, changed = util.SetHash(instance.Status.Hash, common.InputHashName, hash); changed {
 		instance.Status.Hash = hashMap
 		r.Log.Info(fmt.Sprintf("Input maps hash %s - %s", common.InputHashName, hash))
 	}
-	return hash, changed, nil
+	return changed, nil
 }

config/rbac/role.yaml
@@ -54,18 +54,6 @@ rules:
 - apiGroups:
   - ""
   resources:
-  - configmaps
-  verbs:
-  - create
-  - delete
-  - get
-  - list
-  - patch
-  - update
-  - watch
-- apiGroups:
-  - ""
-  resources:
   - pods
   verbs:
   - get

pkg/neutronapi/dbsync.go
@@ -1,7 +1,6 @@
 package neutronapi
 
 import (
-	"github.com/openstack-k8s-operators/lib-common/modules/common/env"
 	neutronv1beta1 "github.com/openstack-k8s-operators/neutron-operator/api/v1beta1"
 	batchv1 "k8s.io/api/batch/v1"
 	corev1 "k8s.io/api/core/v1"
@@ -14,16 +13,11 @@ func DbSyncJob(
 	labels map[string]string,
 	annotations map[string]string,
 ) *batchv1.Job {
-
 	dbSyncExtraMounts := []neutronv1beta1.NeutronExtraVolMounts{}
-
 	runAsUser := int64(0)
-	initVolumeMounts := GetInitVolumeMounts(dbSyncExtraMounts, DbsyncPropagation)
+
 	volumeMounts := GetVolumeMounts("db-sync", dbSyncExtraMounts, DbsyncPropagation)
 	volumes := GetVolumes(cr.Name, dbSyncExtraMounts, DbsyncPropagation)
-
-	envVars := map[string]env.Setter{}
-	envVars["KOLLA_CONFIG_STRATEGY"] = env.SetValue("COPY_ALWAYS")
 
 	job := &batchv1.Job{
 		ObjectMeta: metav1.ObjectMeta{
@@ -39,12 +33,13 @@ func DbSyncJob(
 					ServiceAccountName: cr.RbacResourceName(),
 					Containers: []corev1.Container{
 						{
-							Name:  cr.Name + "-db-sync",
-							Image: cr.Spec.ContainerImage,
+							Command: []string{"neutron-db-manage"},
+							Args:    []string{"upgrade", "heads"},
+							Name:    cr.Name + "-db-sync",
+							Image:   cr.Spec.ContainerImage,
 							SecurityContext: &corev1.SecurityContext{
 								RunAsUser: &runAsUser,
 							},
-							Env:          env.MergeEnvs([]corev1.EnvVar{}, envVars),
 							VolumeMounts: volumeMounts,
 						},
 					},
@@ -53,17 +48,5 @@ func DbSyncJob(
 			},
 		},
 	}
-
-	initContainerDetails := InitContainer{
-		ContainerImage:       cr.Spec.ContainerImage,
-		DatabaseHost:         cr.Status.DatabaseHostname,
-		DatabaseUser:         cr.Spec.DatabaseUser,
-		Database:             Database,
-		NeutronSecret:        cr.Spec.Secret,
-		DBPasswordSelector:   cr.Spec.PasswordSelectors.Database,
-		UserPasswordSelector: cr.Spec.PasswordSelectors.Service,
-		VolumeMounts:         initVolumeMounts,
-	}
-	job.Spec.Template.Spec.InitContainers = GetInitContainer(initContainerDetails)
 	return job
 }

pkg/neutronapi/deployment.go
@@ -25,8 +25,7 @@ import (
 )
 
 const (
-	// ServiceCommand -
-	ServiceCommand = "/usr/local/bin/kolla_set_configs && /usr/local/bin/kolla_start"
+	ServiceCommand = "/usr/bin/neutron-server"
 )
 
 // Deployment func
@@ -50,10 +49,12 @@ func Deployment(
 		PeriodSeconds:       5,
 		InitialDelaySeconds: 20,
 	}
+	cmd := ServiceCommand
+	args := []string{}
 
-	args := []string{"-c"}
 	if instance.Spec.Debug.Service {
-		args = append(args, common.DebugCommand)
+		cmd = "/bin/sleep"
+		args = []string{"infinity"}
 		livenessProbe.Exec = &corev1.ExecAction{
 			Command: []string{
 				"/bin/true",
@@ -66,7 +67,6 @@ func Deployment(
 			},
 		}
 	} else {
-		args = append(args, ServiceCommand)
 		//
 		// https://kubernetes.io/docs/tasks/configure-pod-container/configure-liveness-readiness-startup-probes/
 		//
@@ -81,7 +81,6 @@ func Deployment(
 	}
 
 	envVars := map[string]env.Setter{}
-	envVars["KOLLA_CONFIG_STRATEGY"] = env.SetValue("COPY_ALWAYS")
 	envVars["CONFIG_HASH"] = env.SetValue(configHash)
 
 	deployment := &appsv1.Deployment{
@@ -103,12 +102,10 @@ func Deployment(
 					ServiceAccountName: instance.RbacResourceName(),
 					Containers: []corev1.Container{
 						{
-							Name: ServiceName + "-api",
-							Command: []string{
-								"/bin/bash",
-							},
-							Args:  args,
-							Image: instance.Spec.ContainerImage,
+							Name:    ServiceName + "-api",
+							Command: []string{cmd},
+							Args:    args,
+							Image:   instance.Spec.ContainerImage,
 							SecurityContext: &corev1.SecurityContext{
 								RunAsUser: &runAsUser,
 							},
@@ -138,19 +135,6 @@ func Deployment(
 	if instance.Spec.NodeSelector != nil && len(instance.Spec.NodeSelector) > 0 {
 		deployment.Spec.Template.Spec.NodeSelector = instance.Spec.NodeSelector
 	}
-
-	initContainerDetails := InitContainer{
-		ContainerImage:       instance.Spec.ContainerImage,
-		DatabaseHost:         instance.Status.DatabaseHostname,
-		DatabaseUser:         instance.Spec.DatabaseUser,
-		Database:             Database,
-		NeutronSecret:        instance.Spec.Secret,
-		TransportURLSecret:   instance.Status.TransportURLSecret,
-		DBPasswordSelector:   instance.Spec.PasswordSelectors.Database,
-		UserPasswordSelector: instance.Spec.PasswordSelectors.Service,
-		VolumeMounts:         GetInitVolumeMounts(instance.Spec.ExtraMounts, NeutronAPIPropagation),
-	}
-	deployment.Spec.Template.Spec.InitContainers = GetInitContainer(initContainerDetails)
 
 	return deployment
 }

pkg/neutronapi/initcontainer.go
@@ -1,97 +1,0 @@
-package neutronapi
-
-import (
-	corev1 "k8s.io/api/core/v1"
-)
-
-// InitContainer information
-type InitContainer struct {
-	Privileged           bool
-	ContainerImage       string
-	Database             string
-	DatabaseHost         string
-	DatabaseUser         string
-	NeutronSecret        string
-	TransportURLSecret   string
-	DBPasswordSelector   string
-	UserPasswordSelector string
-	VolumeMounts         []corev1.VolumeMount
-}
-
-// GetInitContainer - init container for neutron services
-func GetInitContainer(init InitContainer) []corev1.Container {
-	runAsUser := int64(0)
-	trueVar := true
-
-	securityContext := &corev1.SecurityContext{
-		RunAsUser: &runAsUser,
-	}
-	if init.Privileged {
-		securityContext.Privileged = &trueVar
-	}
-
-	envs := []corev1.EnvVar{
-		{
-			Name:  "DatabaseHost",
-			Value: init.DatabaseHost,
-		},
-		{
-			Name:  "Database",
-			Value: init.Database,
-		},
-		{
-			Name:  "DatabaseUser",
-			Value: init.DatabaseUser,
-		},
-		{
-			Name: "DatabasePassword",
-			ValueFrom: &corev1.EnvVarSource{
-				SecretKeyRef: &corev1.SecretKeySelector{
-					LocalObjectReference: corev1.LocalObjectReference{
-						Name: init.NeutronSecret,
-					},
-					Key: init.DBPasswordSelector,
-				},
-			},
-		},
-		{
-			Name: "NeutronPassword",
-			ValueFrom: &corev1.EnvVarSource{
-				SecretKeyRef: &corev1.SecretKeySelector{
-					LocalObjectReference: corev1.LocalObjectReference{
-						Name: init.NeutronSecret,
-					},
-					Key: init.UserPasswordSelector,
-				},
-			},
-		},
-	}
-
-	if init.TransportURLSecret != "" {
-		envTransport := corev1.EnvVar{
-			Name: "TransportURL",
-			ValueFrom: &corev1.EnvVarSource{
-				SecretKeyRef: &corev1.SecretKeySelector{
-					LocalObjectReference: corev1.LocalObjectReference{
-						Name: init.TransportURLSecret,
-					},
-					Key: "transport_url",
-				},
-			},
-		}
-		envs = append(envs, envTransport)
-	}
-
-	return []corev1.Container{
-		{
-			Name:            "init",
-			Image:           init.ContainerImage,
-			SecurityContext: securityContext,
-			Command: []string{
-				"/bin/bash", "-c", "/usr/local/bin/container-scripts/init.sh",
-			},
-			Env:          envs,
-			VolumeMounts: init.VolumeMounts,
-		},
-	}
-}

pkg/neutronapi/volumes.go
@@ -6,68 +6,21 @@ import (
 	corev1 "k8s.io/api/core/v1"
 )
 
-// GetInitVolumeMounts - Nova Control Plane init task VolumeMounts
-func GetInitVolumeMounts(extraVol []neutronv1beta1.NeutronExtraVolMounts, svc []storage.PropagationType) []corev1.VolumeMount {
-	vm := []corev1.VolumeMount{
-		{
-			Name:      "scripts",
-			MountPath: "/usr/local/bin/container-scripts",
-			ReadOnly:  true,
-		},
-		{
-			Name:      "config-data",
-			MountPath: "/var/lib/config-data/default",
-			ReadOnly:  true,
-		},
-		{
-			Name:      "config-data-merged",
-			MountPath: "/var/lib/config-data/merged",
-			ReadOnly:  false,
-		},
-	}
-	for _, exv := range extraVol {
-		for _, vol := range exv.Propagate(svc) {
-			vm = append(vm, vol.Mounts...)
-		}
-	}
-	return vm
-}
-
 // GetVolumes -
 // TODO: merge to GetVolumes when other controllers also switched to current config
 //
 //	mechanism.
 func GetVolumes(name string, extraVol []neutronv1beta1.NeutronExtraVolMounts, svc []storage.PropagationType) []corev1.Volume {
-	var scriptsVolumeDefaultMode int32 = 0755
 	var config0640AccessMode int32 = 0640
 
 	res := []corev1.Volume{
 		{
-			Name: "scripts",
+			Name: "config",
 			VolumeSource: corev1.VolumeSource{
-				ConfigMap: &corev1.ConfigMapVolumeSource{
-					DefaultMode: &scriptsVolumeDefaultMode,
-					LocalObjectReference: corev1.LocalObjectReference{
-						Name: name + "-scripts",
-					},
-				},
-			},
-		},
-		{
-			Name: "config-data",
-			VolumeSource: corev1.VolumeSource{
-				ConfigMap: &corev1.ConfigMapVolumeSource{
+				Secret: &corev1.SecretVolumeSource{
 					DefaultMode: &config0640AccessMode,
-					LocalObjectReference: corev1.LocalObjectReference{
-						Name: name + "-config-data",
-					},
+					SecretName:  name + "-config",
 				},
-			},
-		},
-		{
-			Name: "config-data-merged",
-			VolumeSource: corev1.VolumeSource{
-				EmptyDir: &corev1.EmptyDirVolumeSource{Medium: ""},
 			},
 		},
 	}
@@ -84,23 +37,11 @@ func GetVolumes(name string, extraVol []neutronv1beta1.NeutronExtraVolMounts, sv
 func GetVolumeMounts(serviceName string, extraVol []neutronv1beta1.NeutronExtraVolMounts, svc []storage.PropagationType) []corev1.VolumeMount {
 	res := []corev1.VolumeMount{
 		{
-			Name:      "scripts",
-			MountPath: "/usr/local/bin/container-scripts",
-			ReadOnly:  true,
-		},
-		{
-			Name:      "config-data-merged",
-			MountPath: "/var/lib/config-data/merged",
-			ReadOnly:  false,
-		},
-		{
-			Name:      "config-data-merged",
-			MountPath: "/var/lib/kolla/config_files/config.json",
-			SubPath:   serviceName + "-config.json",
+			Name:      "config",
+			MountPath: "/etc/neutron.conf.d",
 			ReadOnly:  true,
 		},
 	}
-
 	for _, exv := range extraVol {
 		for _, vol := range exv.Propagate(svc) {
 			res = append(res, vol.Mounts...)

templates/neutronapi/bin/bootstrap.sh
@@ -1,20 +1,0 @@
-#!/bin/bash
-#
-# Copyright 2022 Red Hat Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License"); you may
-# not use this file except in compliance with the License. You may obtain
-# a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-# License for the specific language governing permissions and limitations
-# under the License.
-set -ex
-
-OPTS="--config-file /etc/neutron/neutron.conf --config-file /etc/neutron/plugins/ml2/ml2_conf.ini"
-neutron-db-manage ${OPTS} upgrade heads
-exit 0

templates/neutronapi/bin/init.sh
@@ -1,60 +1,0 @@
-#!/bin/bash
-#
-# Copyright 2022 Red Hat Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License"); you may
-# not use this file except in compliance with the License. You may obtain
-# a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-# License for the specific language governing permissions and limitations
-# under the License.
-set -ex
-
-# This script generates the nova.conf/logging.conf file and
-# copies the result to the ephemeral /var/lib/config-data/merged volume.
-#
-# Secrets are obtained from ENV variables.
-export DB=${Database:-"neutron"}
-export PASSWORD=${NeutronPassword:?"Please specify a NeutronPassword variable."}
-export DBHOST=${DatabaseHost:?"Please specify a DatabaseHost variable."}
-export DBUSER=${DatabaseUser:?"Please specify a DatabaseUser variable."}
-export DBPASSWORD=${DatabasePassword:?"Please specify a DatabasePassword variable."}
-export TRANSPORTURL=${TransportURL:-""}
-
-function merge_config_dir {
-    echo merge config dir $1
-    for conf in $(find $1 -type f); do
-        conf_base=$(basename $conf)
-
-        # If CFG already exist in ../merged and is not a json file,
-        # Else, just copy the full file.
-        if [[ -f /var/lib/config-data/merged/${conf_base} && ${conf_base} != *.json ]]; then
-            echo merging ${conf} into /var/lib/config-data/merged/${conf_base}
-            crudini --merge /var/lib/config-data/merged/${conf_base} < ${conf}
-        else
-            echo copy ${conf} to /var/lib/config-data/merged/
-            cp -f ${conf} /var/lib/config-data/merged/
-        fi
-    done
-}
-
-# Copy default service config from container image as base
-cp -a /etc/neutron/neutron.conf /var/lib/config-data/merged/neutron.conf
-
-# merge config files if the config mount exists
-if [[ -d /var/lib/config-data/default ]]; then
-    merge_config_dir /var/lib/config-data/default
-fi
-
-# set secrets
-if [ -n "$TRANSPORTURL" ]; then
-    crudini --set /var/lib/config-data/merged/neutron.conf DEFAULT transport_url $TRANSPORTURL
-fi
-crudini --set /var/lib/config-data/merged/neutron.conf database connection mysql+pymysql://${DBUSER}:${DBPASSWORD}@${DBHOST}/${DB}
-crudini --set /var/lib/config-data/merged/neutron.conf keystone_authtoken password $PASSWORD
-crudini --set /var/lib/config-data/merged/neutron.conf nova password $PASSWORD

commit 452f07fcc6a438061f7ac923868ba87de691080c

Wait for db-sync completion before creating neutronapi Deployment
After recent changes that removed kolla from the startup path, the code that
calculated new hash values was moved from the main reconcileNormal into
reconcileInit subroutine. This made immediate return on input hash change from
reconcileInit not to circumvert further loop execution, as was before the
patch, but instead to proceed with the execution of the "main" procedure, incl.
creating the neutronapi Deployment.

This behavior resulted in neutronapi startup before the db was initialized,
which could be observed as failure of the API container with error messages
about missing tables in neutron database.

This patch explicitly requests a Requeue on input hash change, which stops the
current loop execution before the Deployment is created.

Note: it is unclear how to validate the fix in test environment because what
we'd have to validate is whether the reconcile loop did NOT create a
Deployment, and it's not clear how to be sure that the code had a chance to do
it but decided not to, comparing to a case where the reconcile was slower than
the test scenario. Hence, leaving the patch without new test coverage.

Resolves: https://issues.redhat.com/browse/OSP-29534

Fixes: 7a750f104a ("Remove kolla bits from service configuration path")
Signed-off-by: Ihar Hrachyshka <ihrachys@redhat.com>

controllers/neutronapi_controller.go
@@ -366,7 +366,7 @@ func (r *NeutronAPIReconciler) reconcileInit(
 	} else if hashChanged {
 		// Hash changed and instance status should be updated (which will be done by main defer func),
 		// so we need to return and reconcile again
-		return ctrl.Result{}, nil
+		return ctrl.Result{Requeue: true}, nil
 	}
 	// Create Secrets - end
 