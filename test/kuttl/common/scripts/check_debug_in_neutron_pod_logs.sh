9fcb6952383422844d558ef34cce55c79dcffcb7
Yatin Karel <ykarel@redhat.com>
2024-09-05 08:31:30 -0400

    Add kuttl tests to validate config overrides with extraMounts
    Added config override check for both secret and configmap.
    
    Also update debug config check pod to filter only running containers.
    
    Resolves: OSPRH-4296

--- test/kuttl/common/scripts/check_debug_in_neutron_pod_logs.sh
+++ test/kuttl/common/scripts/check_debug_in_neutron_pod_logs.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-pod=$(oc get pods -n $NAMESPACE -l service=neutron -o name)
+pod=$(oc get pods -n $NAMESPACE -l service=neutron --field-selector=status.phase=Running -o name|head -1)
 
 # Check if the neutron pod logs contain DEBUG messages
 oc logs -n $NAMESPACE "$pod" | grep -q "DEBUG"
